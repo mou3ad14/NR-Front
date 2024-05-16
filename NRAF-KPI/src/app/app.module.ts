cd539c683f7cf6dcffdcdf349026358337d53158
mou3ad14 <elmanssourimouad657@gmail.com>
2024-05-16 15:29:20 +0100

    refact

--- NRAF-KPI/src/app/app.module.ts
+++ NRAF-KPI/src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +29,14 @@ import { HttpClientModule } from '@angular/common/http';
     NoopAnimationsModule,
     DragDropModule,
     MatProgressBarModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- NRAF-KPI/src/app/card/card.component.ts
+++ NRAF-KPI/src/app/card/card.component.ts
@@ -1,9 +1,15 @@
-  import { Component, OnInit } from '@angular/core';
+  import { OnInit } from '@angular/core';
   import { CdkDragDrop,moveItemInArray ,transferArrayItem} from '@angular/cdk/drag-drop';
   import { Data } from '@angular/router';
   import { DataService } from '../data.service';
   import { HttpClient } from '@angular/common/http';
+  import { BreakpointObserver } from '@angular/cdk/layout';
+  import { MatSidenav } from '@angular/material/sidenav';
 
+  import {
+    Component,
+    ViewChild,
+  } from '@angular/core';
   @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
@@ -12,11 +18,25 @@
   export class CardComponent implements OnInit {
     data: Data = {};
 
+    title = 'material-responsive-sidenav';
+    isCollapsed = true;
 
-    constructor(private http: HttpClient) { }
+
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  isMobile= true;
+
+    constructor(private http: HttpClient,private observer: BreakpointObserver) { }
 
     ngOnInit(): void {
       this.getDataById(1);
+      this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+        if(screenSize.matches){
+          this.isMobile = true;
+        } else {
+          this.isMobile = false;
+        }
+      });
     }
     getDataById(id: number): void {
       this.http.get<Data>('http://localhost:8080/data/getById?id=' + id)
@@ -30,7 +50,16 @@
         );
     }
 
-  
+    toggleMenu() {
+      if(this.isMobile){
+        this.sidenav.toggle();
+        this.isCollapsed = false; // On mobile, the menu can never be collapsed
+      } else {
+        this.sidenav.open(); // On desktop/tablet, the menu can never be fully closed
+        this.isCollapsed = !this.isCollapsed;
+      }
+    }
+
     
   
   }
